2f270654a91d4e0a22c1acfe06f32a4a3e290c5a
Roni Lazimi <rlazimi.dev@gmail.com>
2026-05-08 07:25:03 -0400

    cloudflare analytics

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "79b3ca9075264ca18876428411998a66"}'
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
